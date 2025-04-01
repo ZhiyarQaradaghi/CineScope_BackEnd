e246066699b2058ec2b26a8a4d1e7d49e214ebe4
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-04-01 03:04:04 +0300

    Enhance CORS configuration and add test route

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://cinescope-kappa.vercel.app", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -18,6 +23,7 @@ app.use("/api/favorites", require("./routes/favoriteRoutes"));
 app.use("/api/tv", require("./routes/tvRoutes"));
 app.use("/api/search", require("./routes/searchRoutes"));
 app.use("/api/genres", require("./routes/genreRoutes"));
+app.use("/api/test", require("./routes/testRoutes"));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok", message: "Server is running" });
